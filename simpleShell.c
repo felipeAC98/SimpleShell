717036b664821cc098d146426d2be98527ec42d8
FelipeAC98 <felipe.alvcor@gmail.com>
2021-09-07 15:54:45 -0300

    Obtendo de melhor forma caracteres do shell

--- simpleShell.c
+++ simpleShell.c
@@ -2,18 +2,37 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include<signal.h>
+
+//https://brennan.io/2015/01/16/write-a-shell-in-c/
+
+int leituraTerminal(char *buffer);
+char interpretaEntrada(char *buffer, int tamanhoEntrada, char *prog);
 
 int main()
 { 
+	pid_t shell_pgid;
 	pid_t result;
-	char comandoShell[30];
+	int shell_terminal = STDIN_FILENO;
 
 	while(1){
 
-	    //Aguarda um comando para executar
-	    scanf("%s", comandoShell);
+		//Inserindo o shell em foreground - vai ficar preso aqui enquanto nao for fg
+		while (tcgetpgrp (shell_terminal) != (shell_pgid = getpgrp ())){
+			kill (- shell_pgid, SIGTTIN);
+		}
 
-		// pid_t vfork(void);
+		//Alocando um buffer dinamico para guardar a entrada do terminal	
+		char *buffer = malloc(sizeof(char) * 1024);
+		int tamanhoEntrada = leituraTerminal(&buffer);
+
+		//variavel na qual vai ser salvo o programa digitado, futuramente sera substituido por uma matriz de argumentos tipo argv
+		char prog[tamanhoEntrada];
+
+		//faz a separacao dos comandos e parametros recebidos
+		interpretaEntrada(&buffer, tamanhoEntrada,  &prog);
+
+		// faz o fork do processo
 		result=vfork();
 
 		if(result==-1) {
@@ -22,8 +41,8 @@ int main()
 		}
 
 		if(!result) {  // filho
-			/* sobrepoe o processo atual com um novo processo */
-			result=execlp(comandoShell,"hello",NULL);
+			//passando para o filho o comando que deve ser executado
+			result=execlp(prog,prog,NULL);
 
 			perror("Erro em execl");
 			exit(0);
@@ -31,10 +50,46 @@ int main()
 		}else {  // pai
 
 			//este wait faz com que o pai espere o filho terminar, logo o processo filho estara rodando em foreground
-			wait();
+			waitpid(result, NULL, 0);
 			//printf("Pai retomou a execucao.\n");
 			printf("\n");
 		}
 
 	}
+}
+
+int leituraTerminal(char *buffer){
+
+	//alocando um buffer na memoria
+	int c;
+	int tamanhoEntrada=0;
+
+	printf("$ ");
+
+	while(1){
+		c = getchar();
+
+		//inserindo no buffer na posicao "tamanhoEntrada" o proximo caracter lido
+		buffer[tamanhoEntrada] = c;
+
+		if(c == EOF || c=='\n'){
+			return tamanhoEntrada;
+		}
+
+		tamanhoEntrada++;
+
+	}
+
+}
+
+char interpretaEntrada(char *buffer, int tamanhoEntrada, char *prog){
+
+	for(int i=0; i<tamanhoEntrada; i++){
+		prog[i]=buffer[i];
+	}
+
+	//printf("tamanhoEntrada: %d \n",tamanhoEntrada);
+	//printf("buffer: %s \n",buffer);
+
+	return prog;
 }
