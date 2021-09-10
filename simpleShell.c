1495c79e9c3d093880abd9fa4a90ea1561a47427
FelipeAC98 <felipe.alvcor@gmail.com>
2021-09-09 23:21:47 -0300

    Implementacao funcao para separacao das strings

--- simpleShell.c
+++ simpleShell.c
@@ -3,8 +3,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include<signal.h>
+#include <string.h>
 
-int leituraTerminal(char *buffer);
+int leituraTerminal(char *buffer, int *nParametros);
 char interpretaEntrada(char *buffer, int tamanhoEntrada, char *prog);
 
 int main()
@@ -14,6 +15,7 @@ int main()
 	int shell_terminal = STDIN_FILENO;
 
 	while(1){
+		int nParametros=1;
 
 		//Inserindo o shell em foreground - vai ficar preso aqui enquanto nao for fg
 		while (tcgetpgrp (shell_terminal) != (shell_pgid = getpgrp ())){
@@ -22,13 +24,19 @@ int main()
 
 		//Alocando um tamanho maximo para a entrada do terminal - precisa verificar sobre alocacao dinamica
 		char buffer[20];
-		int tamanhoEntrada = leituraTerminal(&buffer);
+		int tamanhoEntrada = leituraTerminal(&buffer, &nParametros);
+		
+		//Criando variavel de argumentos
+		char *argv[nParametros+1];
 
 		//variavel na qual vai ser salvo o programa digitado, futuramente sera substituido por uma matriz de argumentos tipo argv
 		char prog[tamanhoEntrada];
 
+		separaStrings(&buffer, &argv, &prog, tamanhoEntrada, nParametros);
+
+
 		//faz a separacao dos comandos e parametros recebidos
-		interpretaEntrada(&buffer, tamanhoEntrada,  &prog);
+		//interpretaEntrada(&buffer, tamanhoEntrada,  &prog);
 
 		// faz o fork do processo
 		result=vfork();
@@ -40,7 +48,7 @@ int main()
 
 		if(!result) {  // filho
 			//passando para o filho o comando que deve ser executado
-			result=execlp(prog,prog,NULL);
+			result=execlp(prog,prog,NULL);;
 
 			perror("Erro em execl");
 			exit(0);
@@ -56,7 +64,7 @@ int main()
 	}
 }
 
-int leituraTerminal(char *buffer){
+int leituraTerminal(char *buffer, int *nParametros){
 
 	//alocando um buffer na memoria
 	int c;
@@ -71,7 +79,11 @@ int leituraTerminal(char *buffer){
 		buffer[tamanhoEntrada] = c;
 
 		if(c == EOF || c=='\n'){
+			buffer[tamanhoEntrada] = '\0';
 			return tamanhoEntrada;
+		}
+		else if(c == ' '){
+			*nParametros=*nParametros+1;	
 		}
 
 		tamanhoEntrada++;
@@ -90,4 +102,30 @@ char interpretaEntrada(char *buffer, int tamanhoEntrada, char *prog){
 	//printf("buffer: %s \n",buffer);
 
 	return prog;
+}
+
+void separaStrings(char *buffer, char *argv[], char *prog, int tamanhoEntrada, int nParametros){
+
+	int argumentoAtual=0;
+	char delimitador[] = " ";
+	char *parametro = strtok(buffer, delimitador);
+
+
+	while(parametro != NULL)
+	{
+
+		if(argumentoAtual==0)
+			strcpy(prog, parametro);
+		//strcpy(argv[argumentoAtual], parametro);
+		argv[argumentoAtual]=parametro;
+		//printf("argv %s\n",argv[argumentoAtual]);
+
+		argumentoAtual++;
+		parametro = strtok(NULL, delimitador);
+	}
+
+	argv[argumentoAtual]=NULL;
+	//printf("prog: %s\n",prog);
+	//printf("parametro: %s\n",parametro);
+
 }
