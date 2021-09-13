5036adb70f8739594cc54f6b8a88a6849afe6664
FelipeAC98 <felipe.alvcor@gmail.com>
2021-09-13 19:14:22 -0300

    Revert "Separando funções em arquivos"
    This reverts commit 2d40d31709bfb3f2c8e6bab474fd8f3e5a4f3771.

--- auxiliar/aux_func.h
+++
@@ -1,9 +0,0 @@
-#include "aux_func.c"
-#ifndef aux_func
-#define aux_func
-
-extern int leituraTerminal(char *buffer, int *nParametros);
-extern char interpretaEntrada(char *buffer, int tamanhoEntrada, char *prog);
-extern int verificaExecucaoBG(char *argv[], int nParametros);
-
-#endif
